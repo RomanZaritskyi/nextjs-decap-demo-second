4bee583e4fd7383d2627a2dc8543e4fe0b18e870
Roman <romanzaritskyi@gmail.com>
2023-07-21 18:12:41 +0300

    wwwww

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 import Header from "@/components/Header/Header";
 import Banner from "@/components/Banner/Banner";
 
+import { attributes, react as HomeContent } from "../content/home.md";
+
 export default function Home() {
+  console.log(attributes);
   return (
     <>
       <div className='main-wrapper'>
